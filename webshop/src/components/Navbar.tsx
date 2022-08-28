dd40a0d26250cfd7607927808a411e50704eedca
osci-the-orca <88oscste@skola.boras.se>
2022-08-28 17:47:26 +0200

    created navbar component also with styled components

---
+++ webshop/src/components/Navbar.tsx
@@ -0,0 +1,92 @@
+import { Search } from "@mui/icons-material";
+import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
+import { Badge } from "@mui/material";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
+// LEFT IN NAV BAR
+const Left = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+const Language = styled.span`
+  font-size: 14;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  border: 1px solid lightgray;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+//CENTER IN NAVBAR
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+// RIGHT IN NAVBAR
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex: 1;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14;
+  cursor: pointer;
+  margin: 2rem;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>.web SHOP</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartCheckoutIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
